c6abd799470acbbd4322e38cb2bb40d11cfb5e15
xxavierargentino <xxavierargentino@hotmail.com>
2020-09-05 21:40:53 -0300

    package.json retoque

--- dist/routes/routes.js
+++ dist/routes/routes.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.Routes = void 0;
 var UsuarioController_1 = require("../controller/UsuarioController");
 exports.Routes = [{
         method: "get",
